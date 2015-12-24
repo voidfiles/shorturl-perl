845110688958d0513c68311661a0a47ba42b9c52
Alex Kessinger <akessinger@bepress.com>
2015-12-24 10:31:50 -0800

    It works

--- cpanfile
+++ cpanfile
@@ -2,3 +2,4 @@ requires 'Mojolicious', '6.36';
 requires 'Plack', '1.0000';
 requires 'Redis', '1.981';
 requires 'JSON::Schema', '0.016';
+requires 'Hashids', '1.000002';
--- short.pm
+++ short.pm
@@ -1,26 +1,49 @@
 use Mojolicious::Lite;
 use Redis;
 use Shorturl::Schema qw(urlSchema);
+use Hashids;
 
-get '/' => sub {
-  my $c    = shift;
-  my $host = $c->req->url->to_abs->host;
-  my $ua   = $c->req->headers->user_agent;
-  $c->render(text => 'I ♥ Mojolicious!');
+my $hashids = Hashids->new('random');
+
+get '/:url_id' => sub {
+  my $client    = Redis->new;
+  my $c         = shift;
+  my $hash      = $c->param('url_id');
+  my $unique_id = $hashids->decode($hash);
+  my $url       = $client->get("url:${unique_id}");
+
+  if (!$url) {
+    return $c->reply->not_found;
+  }
+
+  return $c->redirect_to($url);
 };
 
 post '/' => sub {
-  my $c      = shift;
-  my $valid  = urlSchema()->validate($c->req->json);
+  my $c     = shift;
+  my $url   = $c->req->json;
+  my $host  = $c->req->url->to_abs->host;
+  my $valid = urlSchema()->validate($url);
+
   if ($valid) {
-    $c->render(text => 'I ♥ Mojolicious!');
-    # do something interesting here with $dt...
+    my $client      = Redis->new;
+    my $new_url_id = $client->incr( 'unique_url_id' );
+    my $hash       = $hashids->encode($new_url_id);
+    my $url_key    = "url:${new_url_id}";
+
+    $client->set($url_key => $url->{url});
+
+    $c->render(json => {
+      url => 'http://' . $host . '/' . $hash
+    });
+
   } else {
     my $errors = {};
     foreach $a ($valid->errors) {
       $errors->{$a->{property}} = $a->{message};
     }
     $c->render(json => $errors);
+
   }
 };
 
